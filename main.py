a83d673578691b922e1104b7499629fd0aa78bbe
shivam <jhodshivam5122@gmail.com>
2023-04-06 01:17:49 +0530

    python

---
+++ main.py
@@ -0,0 +1,11 @@
+# Tutorial on Return values 
+
+def beef():
+    print('Damn, function are cool')
+
+def bitcoin_to_usd(btc):
+    amount = btc * 527
+    print(amount)
+    
+beef()
+bitcoin_to_usd(3.85)
